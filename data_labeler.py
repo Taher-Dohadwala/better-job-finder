c44785afdecc3141448ed7913badb92b43279b09
Taher Dohadwala <taher.dohadwala1@gmail.com>
2021-06-03 08:35:16 -0500

    ready for labeling

--- data_labeler.py
+++ data_labeler.py
@@ -35,18 +35,18 @@ def find_continue_point(df):
     if not t.any():
         start = 0
     else:
-        start = int(start.tolist()[0])
+        start = int(t.tolist()[0])
     return start
 
 
 start = find_continue_point(df)
-
+print(f"Starting at {start}")
 jobs_to_label = df["Job Description"]
 current = start
 
 try:
     # display job and ask for label
-    for idx,job in enumerate(jobs_to_label):
+    for idx,job in enumerate(jobs_to_label[start:]):
         current = start + idx
         print(yellow(f"Example number: {current}"))
         print(job)
@@ -57,4 +57,5 @@ try:
 except KeyboardInterrupt:
     print(blue(f"ENDING AT: {current}"))
     print(green("SAVING LABELING RESULTS"))
+    df['Label'][current] = 999
     df.to_csv("data/combined_data_withlabel.csv")
