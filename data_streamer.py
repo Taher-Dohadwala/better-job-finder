1c37eb4d638d2c306169ceff3eda24338c610227
Taher Dohadwala <taher.dohadwala1@gmail.com>
2021-06-07 17:49:35 -0500

    almost finished scraping Indeed

--- data_streamer.py
+++ data_streamer.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+import pandas as pd
 
 """
 DataSource_
@@ -9,43 +11,55 @@ Scraped Information:
     - Company
     - Location
     - Posted date
-    - Apply link
+    - Apply link --- STILL NEED
     - Job Description
 """
-class DataSource_Monster():
+class DataSource_Indeed():
     
     def __init__(self):
-        self.base_url = "https://www.monster.com/jobs/search/"
+        self.base_url = 'https://www.indeed.com/jobs?q={}&l={}'
         self.data = {}
-        self.keywords = None
-        self.location = None
-        self.search_url = f"{self.base_url}?q=&where="
+        self.search_url = ""
         self.soup = None
+        
+        self.titles = []
+        self.companies = []
+        self.locations = []
+        self.dates = []
+        self.descriptions = []
     
-    def set_page(self,keywords,location):
+    def search(self,position,location):
         """Sets job search url, and page, by converting keywords and location into the needed format for Monster search url structure"""
-        self.keywords = keywords.replace(" ","+")
-        self.location = location.replace(" ", "")
-        self.search_url = f"{self.base_url}?q={self.keywords}&where={self.location}"
-        page = requests.get(self.search_url)
-        print(page)
-        self.soup = BeautifulSoup(page.content,"html.parser")
-
+        position = position.replace(" ","+")
+        location = location.replace(" ", "+")
+        self.search_url = self.base_url.format(position,location)
 
     def get_info(self):
-        job_elems = self.soup.find_all('section', class_='card-content')
-        for job_elem in job_elems:
-            title_elem = job_elem.find('h2', class_='title')
-            company_elem = job_elem.find('div', class_='company')
-            location_elem = job_elem.find('div', class_='location')
-            if None in (title_elem, company_elem, location_elem):
-                continue
-            print(title_elem.text.strip())
-            print(company_elem.text.strip())
-            print(location_elem.text.strip())
-            print()
         
- 
+        # request for job search results
+        page = requests.get(self.search_url)
+        self.soup = BeautifulSoup(page.content,"html.parser")
+        
+        # Info for individual job posting
+        cards = self.soup.find_all("div","jobsearch-SerpJobCard")
+        for card in cards:
+            self.titles.append(card.h2.a.get("title"))
+            self.companies.append(card.find('span', 'company').text.strip())
+            self.locations.append(card.find('div', 'recJobLoc').get('data-rc-loc'))
+            self.dates.append(card.find('span', 'date').text)
+        # get full description
+            time.sleep(5)
+            jobURL = "http://indeed.com" + card.h2.a.get('href')
+            job_response = requests.get(jobURL)
+            job_soup = BeautifulSoup(job_response.text,"html.parser")
+            job_description_tag = job_soup.find('div',{'id':'jobDescriptionText'})
+            job_description = job_description_tag.text if job_description_tag else "N/A"
+            self.descriptions.append(job_description)
+    
+    def save_to_csv(self,filename):
+        d = {"Title":self.titles,"Company":self.companies,"Location":self.locations,"Date":self.dates,"Description":self.descriptions}
+        df = pd.DataFrame(data=d)
+        df.to_csv(filename)
 
 # class DataStreamer():
     
@@ -53,8 +67,11 @@ class DataSource_Monster():
 #         self.count = 0
 
 if __name__=='__main__':
-    m = DataSource_Monster()
-    m.set_page("Data Science","Seattle")
-    print(m.search_url)
-    m.get_info()
+    # m = DataSource_Indeed()
+    # m.search("Data Science","Seattle")
+    # m.get_info()
+    # m.save_to_csv("example_indeed_scrape.csv")
+    # print("DATA SCRAPED, AND SAVED")
+    print("indeed is almost finished")
+
     
