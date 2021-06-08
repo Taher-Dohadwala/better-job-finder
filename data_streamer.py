ab418f259697d5b196ba1e128162af99164ae58c
Taher Dohadwala <taher.dohadwala1@gmail.com>
2021-06-08 09:40:32 -0500

    working job search

--- data_streamer.py
+++ data_streamer.py
@@ -17,25 +17,25 @@ Scraped Information:
 class DataSource_Indeed():
     
     def __init__(self):
-        self.base_url = 'https://www.indeed.com/jobs?q={}&l={}'
-        self.data = {}
+        self.base_url = 'https://www.indeed.com/jobs?q={}&l={}&sort=date'
         self.search_url = ""
         self.soup = None
-        
+        self.delay = 0.1
         self.titles = []
         self.companies = []
         self.locations = []
         self.dates = []
+        self.applies = []
         self.descriptions = []
+        
     
-    def search(self,position,location):
+    def set_search(self,position,location):
         """Sets job search url, and page, by converting keywords and location into the needed format for Monster search url structure"""
         position = position.replace(" ","+")
         location = location.replace(" ", "+")
         self.search_url = self.base_url.format(position,location)
 
     def get_info(self):
-        
         # request for job search results
         page = requests.get(self.search_url)
         self.soup = BeautifulSoup(page.content,"html.parser")
@@ -48,30 +48,37 @@ class DataSource_Indeed():
             self.locations.append(card.find('div', 'recJobLoc').get('data-rc-loc'))
             self.dates.append(card.find('span', 'date').text)
         # get full description
-            time.sleep(5)
+            time.sleep(self.delay)
             jobURL = "http://indeed.com" + card.h2.a.get('href')
             job_response = requests.get(jobURL)
+            self.applies.append(jobURL)
             job_soup = BeautifulSoup(job_response.text,"html.parser")
             job_description_tag = job_soup.find('div',{'id':'jobDescriptionText'})
             job_description = job_description_tag.text if job_description_tag else "N/A"
             self.descriptions.append(job_description)
+            
+    def get_data(self):
+        return self.titles,self.companies,self.locations,self.dates,self.applies,self.descriptions
     
     def save_to_csv(self,filename):
-        d = {"Title":self.titles,"Company":self.companies,"Location":self.locations,"Date":self.dates,"Description":self.descriptions}
+        d = {"Title":self.titles,"Company":self.companies,"Location":self.locations,
+             "Date":self.dates,"Apply":self.applies,"Description":self.descriptions}
         df = pd.DataFrame(data=d)
         df.to_csv(filename)
 
-# class DataStreamer():
+class DataStreamer():
     
-#     def __init__(self):
-#         self.count = 0
-
-if __name__=='__main__':
-    # m = DataSource_Indeed()
-    # m.search("Data Science","Seattle")
-    # m.get_info()
-    # m.save_to_csv("example_indeed_scrape.csv")
-    # print("DATA SCRAPED, AND SAVED")
-    print("indeed is almost finished")
-
+    def __init__(self):
+        self.indeed = DataSource_Indeed()
     
+    def search(self, position, location):
+        self.indeed.set_search(position,location)
+        self.indeed.get_info()
+    
+    def get_data(self):
+        job_titles,companies,locations,dates,applies,descriptions = self.indeed.get_data()
+        return job_titles,companies,locations,dates,applies,descriptions
+    
+    def save_to_csv(self,filename):
+        self.indeed.save_to_csv(filename)
+ 
--- recommendation_platform.py
+++ recommendation_platform.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import time
-
+from data_streamer import DataStreamer
 # Example data as test
 # https://www.monster.com/job-openings/lead-data-science-analyst-riverwoods-il--1d0c6760-e0b4-45df-b8e3-edd37a7028dc
 job_title = "Lead Data Science Analyst"
@@ -51,8 +51,12 @@ The same way we treat our employees is how we treat all applicants – with resp
 
 
 button_states = {}
+data_streamer = DataStreamer()
+
+
+
 # This function contains a boilerplate job posting layout
-def search_result_block(job_title,company,location_,date,description,block_id):
+def search_result_block(job_title,company,location_,date,apply,description,block_id):
     col1, col2 = st.beta_columns(2)
     with col1:
         st.text(job_title)
@@ -69,7 +73,13 @@ def search_result_block(job_title,company,location_,date,description,block_id):
 
     with st.beta_expander("See Description"):
         st.markdown(description)
-    
+
+@st.cache(show_spinner=False)
+def search(position,location):
+    data_streamer.search(position,location)
+    job_titles,companies,locations,dates,applies,descriptions = data_streamer.get_data()
+    return job_titles,companies,locations,dates,applies,descriptions
+
     
 def main():
     
@@ -79,19 +89,20 @@ def main():
     # typical job search website UI
     col1, col2 = st.beta_columns(2)
     with col1:
-        job = st.text_input('Job Search', 'Entry Level Data Science')
+        position = st.text_input('Job Search', 'Data Science')
     with col2:
         location = st.text_input("Location","Chicago, IL")
-        
+
     # typical job result
     results = st.beta_container()
+    with st.spinner("Getting Jobs..."):
+        job_titles,companies,locations,dates,applies,descriptions = search(position,location)
+    
     with results:
-        search_result_block(job_title,company,location_,date,description,"b1")
-        search_result_block(job_title,company,location_,date,description,"b2")
-        search_result_block(job_title,company,location_,date,description,"b3")
-        search_result_block(job_title,company,location_,date,description,"b4")
-        
+        for i, (job_title,company,location_,date,apply,description) in enumerate(zip(job_titles,companies,locations,dates,applies,descriptions)):
+            search_result_block(job_title,company,location_,date,apply,description,f"b{i}")
+    
     st.text(f"List of labels from current selection: {button_states['b1']}, {button_states['b2']},{button_states['b3']},{button_states['b4']}")
-        
+            
 if __name__ == '__main__':
     main()
